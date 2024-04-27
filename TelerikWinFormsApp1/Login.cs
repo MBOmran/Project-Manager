3a91bdf40070cf82f513c669d0e38a7b37c0e9cd
MOHAMEDSHABAN203 <mohamed01229324461@gmail.com>
2024-04-27 19:36:16 +0300

    Login And Register Forms Logic Not Yet Completed

---
+++ TelerikWinFormsApp1/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace TelerikWinFormsApp1
+{
+    public partial class Login : Telerik.WinControls.UI.RadForm
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
